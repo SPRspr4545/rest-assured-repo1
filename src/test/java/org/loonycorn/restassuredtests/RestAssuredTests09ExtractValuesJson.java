03651975845a688f9aa411ed48c4bd9c47db283e
sam.pelletier <samuel.pelletier.06.37.57.34.89@gmail.com>
2025-04-10 16:52:28 +0200

    Validating Nested JSON Fields and JSON Arrays

--- src/test/java/org/loonycorn/restassuredtests/RestAssuredTests09ExtractValuesJson.java
+++ src/test/java/org/loonycorn/restassuredtests/RestAssuredTests09ExtractValuesJson.java
@@ -18,14 +18,62 @@ public class RestAssuredTests09ExtractValuesJson {
                 .get(USERS_URL)
                 .prettyPeek()
                 .then()
-                .rootPath("data[0]")
+                .rootPath("data[0]") // je précise le rootPath() index [0] pour accès au 1er objet JSON de la collection "data"
                     .body("id", equalTo(1))
                     .body("first_name", equalTo("George"))
-                .noRootPath()
-                    .body("data.first_name[1]", equalTo("janet.weaver@reqres.in"))
-                    .body("data.email[1]", equalTo("Janet"))
-                    .body("data.last_name", hasItems("Holt", "Morris"))
-                    .body("data.last_name", hasItem(startsWith("Ram")));
+                .noRootPath() // pour valider des propriétés de 1er niveau
+                    .body("data.first_name[1]", equalTo("Janet")) // accède à l'objet JSON index [1]
+                    .body("data.email[1]", equalTo("janet.weaver@reqres.in"))
+                    .body("data.last_name", hasItems("Holt", "Morris")) // vérifier la présence de 2 "last_name" dans la liste
+                    .body("data.last_name", hasItem(startsWith("Ram"))); // vérifie si dans la liste un élément commence par "Ram"
     }
+
+    // jusqu'à présent nous avons validé les réponses en comparant les valeurs de la réponse à une valeur codée en dur
+    // Que se passe t-il si vous souhaitez comparer la valeur d'une réponse à une valeur dynamique qui fait partie de la réponse elle même ?
+
+    @Test
+    public void testBody2() {
+        // le matcher ResponseAwareMatcher prend l'objet de réponse comme argument d'entrée
+        RestAssured
+                .get(USERS_URL)
+                .then()
+                    .body("data.email[0]", response -> // fonction lambda
+                            // j'accède au body de la response, puis au jsonPath puis la propriété "first_name[0]"
+                            containsStringIgnoringCase(response.body().jsonPath().get("data.first_name[0]"))) // vérifie que le prénom est inclus dans l'email
+                            // ...IgnoringCase() car le "first_name" contient une majuscule
+                    .body("data.email[0]", response ->
+                            containsStringIgnoringCase(response.body().jsonPath().get("data.last_name[0]"))); // je fais la même chose pour le "last_name"
+    }
+
+    // affiche la liste des publications
+    private static final String POST_URL = "https://jsonplaceholder.typicode.com/posts";
+    // permet de récupérer les commentaires d'une publication avec un {id}
+    private static final String COMMENT_URL = "https://jsonplaceholder.typicode.com/posts/{id}/comments";
+
+    @Test
+    public void testExtract() {
+        // extraire une partie de la réponse et utiliser
+        Integer postId = RestAssured // stocke l'id qu'on extrait de la réponse dans la variable "postId"
+                .get(POST_URL)
+                //.prettyPeek()
+                .then()
+                    .body("size()", equalTo(100)) // vérifie que le nombre de publication = 100
+                    .extract() // la méthode extract() donne accès à l'objet de réponse...
+                    .path("id[2]"); // ...spécifié par le chemin jsonPath de la valeur que je souhaite extraire
+
+        RestAssured
+                .given()
+                    .pathParam("id", postId) // spécifie le postId sous la forme .pathParam()
+                // j'utilise cet "id" que j'ai extrait de la réponse précédente pour compléter {id} dans l'URL
+                // et  récupérer les commentaires
+                .when()
+                    .get(COMMENT_URL)
+                    .then()
+                    .body("size()", equalTo(5)); // j'effectue une validation de base (taille des commentaire = 5)
+    }
+
+    //Generating JSON Schema for Validation
+
+
 
 }
---
+++ src/test/java/org/loonycorn/restassuredtests/RestAssuredTests10StructureJson.java
@@ -0,0 +1,47 @@
+package org.loonycorn.restassuredtests;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class RestAssuredTests10StructureJson {
+
+    // REST Assured API Testing: Validating JSON Responses & Schemas
+        // Generating JSON Schema for Validation
+        // Validating JSON Schema
+
+    private static final String TODOS_URL = "https://jsonplaceholder.typicode.com/todos/{id}";
+
+    @Test
+    public void testSchemaValidation() {
+        RestAssured
+                .given()
+                    .pathParam("id", 1)
+                .when()
+                    .get(TODOS_URL)
+                .then() // quand la réponse est disponible:
+                    .body(matchesJsonSchemaInClasspath("todo_schema.json")); // vérifie si le body correspond au schéma spécifié
+            // "todo_schema.json" se trouve directement sous "resources", je n'ai pas besoin de spécifier le chemin absolue (complet)
+    }
+
+    // Validating Response Type and Response Properties
+    // Configuring Required Properties and Additional Properties
+    // Specifying Range Constraints for Response Values
+    // Validating Nested JSON Fields and JSON Arrays
+
+    private static final String USERS_URL = "https://reqres.in/api/users/{id}";
+
+    @Test
+    public void testSchemaValidation2() {
+        RestAssured
+                .given()
+                    .pathParam("id", 5)
+                .when()
+                    .get(USERS_URL)
+                .then()
+                    .body(matchesJsonSchemaInClasspath("user_schema.json"));
+
+    }
+
+}
